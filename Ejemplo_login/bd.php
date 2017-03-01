f4ac75bcc7bfee9d8c4d8cdca25aa5b71ea54482
Israelem <israelem@gmail.com>
2017-03-01 21:45:36 +0100

    Add files via upload

--- Ejemplo_login/bd.php
+++ Ejemplo_login/bd.php
@@ -3,25 +3,18 @@
 define('SERVER_DB', '127.0.0.1');
 define('USER_DB', 'root');
 define('PASS_DB', '');
-define('NAME_DB', 'test')
+define('NAME_DB', 'test');
 
 function login($user, $password){
   $db = mysqli_connect(SERVER_DB, USER_DB, PASS_DB, NAME_DB);
   $query = "SELECT * FROM usuario WHERE id = $user";
   $result = mysqli_query($db, $query);
   if ($result && mysqli_num_rows($result) == 1) {
-    $row = mysqli_fetch_assoc($result);
-    if (password_verify($password, $row['password'])) {
-      $return = true;
-    }else{
-        header("location:login.php?error=1");
-    }
+
   }else{
     header("location:login.php?error=1");
   }
   mysqli_close($db);
-
-  return $return;
 }
 
 function createUser($db, $user, $password, $name, $email){
@@ -37,14 +30,10 @@ function createUser($db, $user, $password, $name, $email){
                 VALUES($user, $passhash, $name, $email)";
     $hashpass = password_hash($password, PASSWORD_DEFAULT);
     $ok = mysqli_query($db, $insert);
-    if ($ok) {
-      $return = true;
-    }else{
-      header("location:create.php?error=1");
-    }
+    return true;
   }
   mysqli_close($db);
-  return $return;
 }
+
 
 ?>
